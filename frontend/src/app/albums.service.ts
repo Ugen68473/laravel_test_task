bb4e61a8d0e3ac2eeecc584f6eb296030dc062a7
Ugen <Bydanauyauheni@mail.ru>
2017-03-04 17:43:23 +0300

    Create Http Sevice

---
+++ frontend/src/app/albums.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+
+
+@Injectable()
+export class AlbumsService {
+    constructor(private http: Http) {
+        console.log('it works');
+    }
+
+
+    addAlbum(album: any){
+        const body = JSON.stringify(album);
+        const headers = new Headers({'Content-Type': 'application/json'})
+        return this.http.post('http://localhost/sites/albums/api/album', body, {headers: headers})
+            .map((data:Response) => data.json());
+    }
+
+    getAlbums(){
+        return this.http.get('http://localhost/sites/albums/api/albums')
+            .map((response: Response) => {return response.json().albums;});
+
+    }
+
+
+
+
+
+}
+
