a916b69dfe5ab1e5cdbe194d854429136f7c3ae3
Ugen <Bydanauyauheni@mail.ru>
2017-03-04 17:17:04 +0300

    Add Models Album,Track, Add Migrations

---
+++ backend/database/migrations/2017_03_01_201854_create_tracks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->increments('id_track');
+            $table->integer('id_album');
+            $table->string('musician');
+            $table->string('title_track');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
